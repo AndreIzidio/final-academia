91176717f40d2ffb645815f951c1ba4e5bd0adbc
André <costa.izidio.andre@gmail.com>
2023-04-03 12:09:17 -0300

    imc concluido

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //modo dark
-const header = document.querySelector('body');
-const icon = document.querySelector('.icon')
+//
+//const header = document.querySelector('body');
+//const icon = document.querySelector('.icon')
 
-icon.addEventListener('click' , () => {
-  header.classList.toggle('dark')
-})
+//icon.addEventListener('click' , () => {
+ // header.classList.toggle('dark')
+//})
+//
 
 
 //menu hamburguer
@@ -13,27 +15,45 @@ let menu = document.querySelector('#menu-hamburguer');
 let navbar = document.querySelector('.cabecalho');
 
 menu.onclick = () => {
-
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
 
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+  menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
 }
 
 
 
 //IMC
-function calcularIMC(){
-  const peso = parseFloat(document.getElementById("peso").value);
-  const altura = parseFloat(document.getElementById("altura").value);
+const resultado = document.querySelector('#resultado')
+const altura = document.querySelector('#altura')
+const peso = document.querySelector('#peso')
 
-  const imc = (peso / (altura*altura)).toFixed(1);
+const calcIMC = () => {
 
-  document.querySelector("h1").innerHTML = imc;
+  if (altura.value !== '' && peso.value !== '') {
+    const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
+    let classification = ''
 
+    if (imc < 18.5) {
+      classification = 'Abaixo do peso'
+    } else if (imc < 25) {
+      classification = 'Peso normal'
+    } else if (imc < 30) {
+      classification = 'Acima do peso'
+    } else if (imc < 35) {
+      classification = 'Obesidade Grau I'
+    } else if (imc < 41) {
+      classification = 'Obesidade Grau II'
+    } else {
+      classification = 'Obesidade Grau III'
+    }
+    
+    resultado.innerHTML = `IMC: ${imc} (${classification})`
+  } else {
+    resultado.innerHTML = 'Preencha os campos'
+  }
 }
